906f6811e24e2b83e5ed018d7012530919623327
Krenjeroth <kjs@benguet.gov.ph>
2026-05-04 15:00:53 +0800

    Update
    Fix bug

---
+++ public/app/_nuxt/B5T83qc6.js
@@ -0,0 +1,1 @@
+import{U as n}from"./CxJuP3Nl.js";const u=()=>{const{$sanctumClient:t}=n();return t};export{u};
---
+++ public/app/_nuxt/CUmt7Kii.js
@@ -0,0 +1,1 @@
+import{u as t}from"./D-yOcX7A.js";const a=()=>{const{user:e}=t();return{hasRole:o=>e.value?.roles?.some(s=>s.title.toLowerCase()===o.toLowerCase())}};export{a as u};
