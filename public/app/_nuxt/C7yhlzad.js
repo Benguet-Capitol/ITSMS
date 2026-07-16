de03bc7ecf0a9f83519b9fafb9132d42062b3f89
Krenjeroth <kjs@benguet.gov.ph>
2026-07-16 13:35:52 +0800

    Update

---
+++ public/app/_nuxt/C7yhlzad.js
@@ -0,0 +1,1 @@
+import{u as t}from"./s7G3A3ie.js";const a=()=>{const{user:e}=t();return{hasRole:o=>e.value?.roles?.some(s=>s.title.toLowerCase()===o.toLowerCase())}};export{a as u};
---
+++ public/app/_nuxt/D1uNdtT2.js
@@ -0,0 +1,1 @@
+import{Y as n}from"./B400iQmz.js";const u=()=>{const{$sanctumClient:t}=n();return t};export{u};
