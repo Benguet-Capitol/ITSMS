3e90c4c269f176a89c6da8d3849d2eb2618ab986
Krenjeroth <kjs@benguet.gov.ph>
2026-08-03 12:05:48 +0800

    Patch

---
+++ public/app/_nuxt/Cbnv84zU.js
@@ -0,0 +1,1 @@
+import{E as t}from"./4BGp6xEu.js";const o=()=>{const s=t(),r=n=>s.value?.permissions?.[n]===!0;return{can:r,canAny:(...n)=>n.some(e=>r(e)),canAll:(...n)=>n.every(e=>r(e))}};export{o as u};
