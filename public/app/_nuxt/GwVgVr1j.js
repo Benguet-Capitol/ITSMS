de3abaa704b85c3da7ccf1fbe1901cbd65108e15
Krenjeroth <kjs@benguet.gov.ph>
2026-08-27 15:52:16 +0800

    Patch

---
+++ public/app/_nuxt/GwVgVr1j.js
@@ -0,0 +1,1 @@
+import{u as o}from"./Cv0FEZvY.js";import{aT as a,as as n,aU as s,aw as u}from"./HoC_qy6u.js";import"./Zx7F8Q0N.js";const f=a(()=>{const e=n(),{isAuthenticated:r}=o();if(!r.value)return;const t=e.redirect.onGuestOnly;if(t===void 0)throw new Error("`sanctum.redirect.onGuestOnly` is not defined");if(t===!1)throw s({statusCode:403});return u(t,{replace:!0})});export{f as default};
---
+++ public/app/_nuxt/Zx7F8Q0N.js
@@ -0,0 +1,1 @@
+import{Y as n}from"./HoC_qy6u.js";const u=()=>{const{$sanctumClient:t}=n();return t};export{u};
