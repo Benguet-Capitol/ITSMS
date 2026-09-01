19d80227f4d1880d53410acf0a6c73c26d527715
Krenjeroth <kjs@benguet.gov.ph>
2026-09-01 18:03:20 +0800

    Update

---
+++ public/app/_nuxt/1wE-xFem.js
@@ -0,0 +1,1 @@
+import{ao as t}from"./D88-nVoo.js";const u=()=>{const s=t(),r=n=>s.value?.permissions?.[n]===!0;return{can:r,canAny:(...n)=>n.some(e=>r(e)),canAll:(...n)=>n.every(e=>r(e))}};export{u};
---
+++ public/app/_nuxt/CeJoej1d.js
@@ -0,0 +1,1 @@
+import{Y as n}from"./D88-nVoo.js";const u=()=>{const{$sanctumClient:t}=n();return t};export{u};
